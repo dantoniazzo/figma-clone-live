fea8a3563bdf9a54ca651df7c8fe730e1ffdc79d
dantoniazzo <85890036+dantoniazzo@users.noreply.github.com>
2025-07-20 14:09:08 +0200

    feat: improve connection logic by ensuring both from and to nodes are validated before calculating points

--- src/features/connection/ui/Connection.tsx
+++ src/features/connection/ui/Connection.tsx
@@ -9,7 +9,6 @@ import { findNode } from 'entities/node';
 import type { Connection as ConnectionType } from 'entities/block';
 import type { Group } from 'konva/lib/Group';
 import { getConnectionId } from '../lib';
-import { getLayer } from 'entities/layer';
 
 interface ConnectionProps {
   connection: ConnectionType;
@@ -30,35 +29,17 @@ export const Connection = (props: ConnectionProps) => {
     const toSide = props.connection.toSide;
     // Only handle one type of connection per arrow
     // If this is a "from" connection, draw arrow from the referenced node to this node
-    if (to && fromSide && toSide) {
+    if (to && from && fromSide && toSide) {
+      const fromNode = findNode(id, from);
       const toNode = findNode(id, to);
-      if (toNode && parent) {
+      if (fromNode && toNode) {
         const calculatedPoints = getUpdatedPoints({
-          fromNode: parent as Group,
+          fromNode: fromNode as Group,
           toNode: toNode as Group,
           fromSide,
           toSide,
         });
         setPoints(calculatedPoints);
-      }
-    }
-    if (from && fromSide && toSide) {
-      const fromNode = findNode(id, from);
-      if (fromNode && parent) {
-        const arrows = getLayer(id)?.find(
-          `#${getConnectionId(props.connection.from || '')}`
-        );
-        if (arrows && arrows.length > 0) {
-          const arrow = arrows[0];
-          (arrow as ArrowType).points(
-            getUpdatedPoints({
-              fromNode: fromNode as Group,
-              toNode: parent as Group,
-              fromSide,
-              toSide,
-            })
-          );
-        }
       }
     }
   }, [id, props.connection]);
